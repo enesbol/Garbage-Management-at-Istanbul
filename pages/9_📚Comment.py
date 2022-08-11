11f63268682238998bcab85a0799369674c215fa
Enes Bol <76845631+enesbol@users.noreply.github.com>
2022-08-11 16:41:16 +0300

    expander deleted.

--- pages/9_📚Comment.py
+++ pages/9_📚Comment.py
@@ -28,33 +28,33 @@ comments = db.collect(conn)
 
 # Show comments
 
-with st.expander("💬 Open comments"):
 
-    # Show comments
 
-    st.write("**Comments:**")
+# Show comments
 
-    for index, entry in enumerate(comments.itertuples()):
-        st.markdown(COMMENT_TEMPLATE_MD.format(entry.name, entry.date, entry.comment))
+st.write("**Comments:**")
 
-        is_last = index == len(comments) - 1
-        is_new = "just_posted" in st.session_state and is_last
-        if is_new:
-            st.success("☝️ Your comment was successfully posted.")
+for index, entry in enumerate(comments.itertuples()):
+    st.markdown(COMMENT_TEMPLATE_MD.format(entry.name, entry.date, entry.comment))
 
-    space(2)
+    is_last = index == len(comments) - 1
+    is_new = "just_posted" in st.session_state and is_last
+    if is_new:
+        st.success("☝️ Your comment was successfully posted.")
 
-    # Insert comment
+space(2)
 
-    st.write("**Add your own comment:**")
-    form = st.form("comment")
-    name = form.text_input("Name")
-    comment = form.text_area("Comment")
-    submit = form.form_submit_button("Add comment")
+# Insert comment
 
-    if submit:
-        date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-        db.insert(conn, [[name, comment, date]])
-        if "just_posted" not in st.session_state:
-            st.session_state["just_posted"] = True
-        st.experimental_rerun()
+st.write("**Add your own comment:**")
+form = st.form("comment")
+name = form.text_input("Name")
+comment = form.text_area("Comment")
+submit = form.form_submit_button("Add comment")
+
+if submit:
+    date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    db.insert(conn, [[name, comment, date]])
+    if "just_posted" not in st.session_state:
+        st.session_state["just_posted"] = True
+    st.experimental_rerun()
